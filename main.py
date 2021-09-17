634636d6336e965b219172ed2f66931525ae7898
Sem441 <semaao@mail.ru>
2021-09-17 18:37:10 +0500

    error correction

--- main.py
+++ main.py
@@ -2,25 +2,21 @@ import datetime
 import random
 
 
-def first_function():
-    print("запущена first_function")
-    return random.randint(0, 10)
+def second_function(function):
 
-
-def second_function(function, file_name):
-
-    def new_function():
-        result = function()
-        with open(f"{file_name}.txt", "w", encoding="utf-8") as file_write:
-            time = datetime.datetime.now()
-            name_func = second_function.__name__
-            attribute = function.__name__
+    def new_function(*args, **kwargs):
+        name_func = second_function.__name__
+        time = datetime.datetime.now()
+        result = function(*args, **kwargs)
+        attribute = function.__name__
+        with open("log.txt", "w", encoding="utf-8") as file_write:
             file_write.write(f"{time} Имя функции: {name_func} Имя атрибута: {attribute} "
                              f"Результат первой функции: {result}\n")
         return result
     return new_function
 
 
-name = input("name: ")
-first_function = second_function(first_function, name)
-first_function()
+@second_function
+def first_function():
+    print("запущена first_function")
+    return random.randint(0, 10)
